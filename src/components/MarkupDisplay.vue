a764462bbf7da7f604987bc65ca900e6df0fe448
Jason M Harrison <jason.m.harrison@gmail.com>
2020-11-20 14:34:17 -0600

    Conditionally display markup info.

--- src/components/MarkupDisplay.vue
+++ src/components/MarkupDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="composed-markup-display">
-    <p>-- We'll display your composed markup here -- </p>
+    <p v-if="showInfoMessage">-- We'll display your composed markup here -- </p>
     <div v-if="headerMarkup" v-html="headerMarkup" />
   </div>
 </template>
@@ -9,6 +9,11 @@
 export default {
   name: 'MarkupDisplay',
   props: ['headerMarkup'],
+  computed: {
+    showInfoMessage() {
+      return !this.headerMarkup;
+    }
+  }
 };
 </script>
 
